bad256b9a761880841a864d2a7968eb33f803326
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-06-07 22:48:42 +0200

    -

--- Hard_Org.py
+++ Hard_Org.py
@@ -8,27 +8,38 @@ current_forder = os.path.dirname(__file__)
 
 
 for filename in os.listdir(current_forder):
-    if filename.endswith((".jpg" , "PNG" , "HEIC")):
+    if filename.endswith((".jpg" , "PNG" , "HEIC" , "heic" , "jpeg" , "png" , "JPG")):
         if not os.path.exists("images"):
             os.mkdir("images")
         shutil.copy((filename), "images")
         os.remove(filename)
-        print('Done')
+        print(' images Done')
     
     
 for filename in os.listdir(current_forder):
-    if filename.endswith((".MP4","mov")):
+    if filename.endswith((".MP4","mov" , "MOV" , "mp4")):
         if not os.path.exists("viedos"):
             os.mkdir("viedos")
         shutil.copy((filename), "viedos")
         os.remove(filename)
-        print('Done')
+        print(' viedos Done')
         
         
 for filename in os.listdir(current_forder):
-    if filename.endswith((".doc","docx")):
+    if filename.endswith((".doc","docx" , "pdf" , "pptx" , "pkg" , "ppt" , "exe" , "PDF" , "txt" , "dmg" , "zip" ,"csv" ,"odt" , "rtf" , "rar" )):
         if not os.path.exists("docs"):
             os.mkdir("docs")
         shutil.copy((filename), "docs")
         os.remove(filename)
-        print('Done')
+        print(' docs Done')
+        
+        
+for filename in os.listdir(current_forder):
+    if filename.endswith((".html" , "svg" , "js" , "css" )):
+        if not os.path.exists("vs_code"):
+            os.mkdir("vs_code")
+        shutil.copy((filename), "vs_code")
+        os.remove(filename)
+        print(' vs_code Done')
+        
+
